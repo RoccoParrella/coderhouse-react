bb80a8826501bfea6d2e4f16ac0afa6709905dd9
Rocco Parrella <businessroccoparrella@gmail.com>
2021-11-04 15:50:51 -0300

    tercer commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
-import NavBar from './main-components/NavBar.js'
-import ItemListContainer from "./main-components/ItemListContainer"
-import Footer from './main-components/Footer.js'
 
 function App() {
   return (
     <>
-      <NavBar/>
-      <div className="divJugadores">
-      <ItemListContainer nombre='Remera' stock="10" precio="500"/>
-      <ItemListContainer nombre='Pantalon' stock="15" precio="650"/>
-      <ItemListContainer nombre='Buzo' stock="3" precio="1500"/>
-      </div>
-      <Footer nombre="Francia"/>
+    
     </>
   );
 }
